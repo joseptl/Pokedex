4c5ca1d4dc4423205ef3b830b4edb444f1eb6eec
Jose Torin <joseptl551@gmail.com>
2021-07-25 11:59:04 -0400

    noScroll

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import SplashScreen from "./SplashScreen";
 import icon from "./pngegg.png";
@@ -11,89 +11,99 @@ export default function Home({ handleGen }) {
     history.push(`/Gen-${(e, el)}`);
     handleGen();
   }
+  const [active, setActive] = useState(false);
+
+  useEffect((e) => {
+    setTimeout((e) => {
+      setActive(true);
+    }, 2000);
+  }, []);
 
   return (
     <>
       <SplashScreen />
-      <section>
-        <article>
-          <h3>Select a generation</h3>
-          <div className="home-list">
-            <div
-              onClick={(e) => {
-                handleClick(e, "I");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>First Gen</h5>
+
+      {active && (
+        <section>
+          <article>
+            <h3>Select a generation</h3>
+            <div className="home-list">
+              <div
+                onClick={(e) => {
+                  handleClick(e, "I");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>First Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "II");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Second Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "III");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Third Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "IV");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Fourth Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "V");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Fifth Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "VI");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Sixth Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "VII");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Seventh Gen</h5>
+              </div>
+              <div
+                onClick={(e) => {
+                  handleClick(e, "VIII");
+                }}
+                className="gen-container"
+              >
+                <img src={icon} alt="pokeball-icon" />
+                <h5>Eighth Gen</h5>
+              </div>
             </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "II");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Second Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "III");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Third Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "IV");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Fourth Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "V");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Fifth Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "VI");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Sixth Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "VII");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Seventh Gen</h5>
-            </div>
-            <div
-              onClick={(e) => {
-                handleClick(e, "VIII");
-              }}
-              className="gen-container"
-            >
-              <img src={icon} alt="pokeball-icon" />
-              <h5>Eighth Gen</h5>
-            </div>
-          </div>
-        </article>
-      </section>
+          </article>
+        </section>
+      )}
     </>
   );
 }
